3affbc53f1d3761adeac56e2a9828328ba184ee1
charlyk6 <123798071+charlyk6@users.noreply.github.com>
2024-04-14 12:40:04 +0300

    фикс подсчета очков

---
+++ darts/ContextDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darts
+{
+    internal class ContextDB : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=DartsDB.db");
+        }
+    }
+}
