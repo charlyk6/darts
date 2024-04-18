000b34a8e0bda9226cdd44ad014e7dca4d052b90
ArtemSaik <artemsaik@gmail.com>
2024-04-18 09:24:34 +0300

    fix

--- darts.db/Entities/ThrowEntity.cs
+++ darts.db/Entities/ThrowEntity.cs
@@ -4,10 +4,18 @@
     {
         public int UsersGameId { get; set; }
         public UsersGameEntity? UsersGame { get; set; }        
+        /// <summary>
+        /// Очки попадания, в зависимости от сектора, 0 - мимо мишени
+        /// </summary>
         public int Sector { get; set; } = 0;
+        /// <summary>
+        /// умножение, 1, 2, 3
+        /// </summary>
         public int Mult { get; set; } = 1;
-
         private int total = 0;
+        /// <summary>
+        /// Рассчитанное количество очков за бросок
+        /// </summary>
         public int Total
         {
             get { return total; }
